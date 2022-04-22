c79876fe075648b356835118d5e83a8d1f0708b4
RomainLAU <romain.laurent@epitech.digital>
2022-04-22 18:40:29 +0200

    adding eventlisteners

--- script.js
+++ script.js
@@ -20,34 +20,54 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    window.addEventListener('scroll', (event) => {
+    let textarea = document.querySelector('#textarea')
 
-        // Swal.fire({
-        //     title: 'Don\'t scroll !!! IT\'S PRIVATE !!!',
-        //     icon: 'error',
-        //     showCancelButton: true,
-        //     cancelButtonText: 'Sorry',
-        //     showConfirmButton: true,
-        //     confirmButtonText: 'Sorry',
-        //     confirmButtonColor: '#d33',
-        //     cancelButtonColor: '#d33',
-        // })
+    textarea.addEventListener('scroll', () => {
+        textarea.setAttribute('rows', 5)
+    })
 
-        alert('don\'t scroll !!')
+    textarea.addEventListener('paste', () => {
+        textarea.style.backgroundColor = "red"
+        textarea.style.color = "white"
+    })
+
+    textarea.addEventListener('mouseenter', () => {
+        textarea.setAttribute('cols', 15)
+    })
+
+    textarea.addEventListener('mouseleave', () => {
+        textarea.setAttribute('cols', 0)
     })
 
     setTimeout(() => {
-        // Swal.fire({
-        //     title: 'Welcome ! Woops, I\'m early... 😅*',
-        //     showCancelButton: false,
-        //     showConfirmButton: false,
-        //     icon: 'info',
-        //     position: 'top',
-        //     timer: 1500,
-        //     timerProgressBar: 'true'
-        // })
+        window.addEventListener('scroll', (event) => {
 
-        alert('woops I\'m early 😅')
+            window.setTimeout(() => {
+                Swal.fire({
+                    title: 'Don\'t scroll !!! IT\'S PRIVATE !!!',
+                    icon: 'error',
+                    showCancelButton: true,
+                    cancelButtonText: 'Sorry',
+                    showConfirmButton: true,
+                    confirmButtonText: 'Sorry',
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#d33',
+                })
+            }, 500)
+        })
+    }, 8000)
+
+
+    setTimeout(() => {
+        Swal.fire({
+            title: 'Welcome ! Woops, I\'m early... 😅*',
+            showCancelButton: false,
+            showConfirmButton: false,
+            icon: 'info',
+            position: 'top',
+            timer: 1500,
+            timerProgressBar: 'true'
+        })
     }, 5000)
 
     let time = document.querySelector('#time')
@@ -78,7 +98,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     time.addEventListener('mouseleave', () => {
         time.style.fontSize = '2em'
-        window.scrollTo(1,document.body.scrollHeight)
+        window.scrollTo(0, 0)
     })
 
     document.addEventListener('mousedown', (event) => {
@@ -93,16 +113,14 @@ window.addEventListener('DOMContentLoaded', () => {
         event.preventDefault()
     })
 
-    const youtubeVideo = document.querySelector('#youtubeVideo')
-
-    youtubeVideo.addEventListener('volumechange', (event) => {
-        alert('aaa')
-    })
-
     let musicPlayer = document.querySelector('#audio')
 
     musicPlayer.addEventListener('timeupdate', () => {
         document.body.style.backgroundColor = "blue"
+    })
+    
+    musicPlayer.addEventListener('volumechange', (event) => {
+        alert('aaa')
     })
 
     musicPlayer.addEventListener('play', () => {
@@ -112,7 +130,6 @@ window.addEventListener('DOMContentLoaded', () => {
     musicPlayer.addEventListener('pause', () => {
         document.body.style.backgroundColor = "white"
     })
-
 
     musicPlayer.addEventListener('volumechange', () => {
         document.body.style.backgroundColor = "lightpink"
@@ -136,23 +153,82 @@ window.addEventListener('DOMContentLoaded', () => {
 
     passwordInput.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
-            alert('No need to press enter :)')
+            Swal.fire({
+                title: 'No need to press enter :)',
+                showCancelButton: false,
+                showConfirmButton: false,
+                icon: 'info',
+                position: 'top',
+                timer: 1500,
+                timerProgressBar: 'true'
+            })
         }
     })
 
     passwordInput.addEventListener('paste', (event) => {
         event.preventDefault()
-        alert('Don\'t try this please...')
+        Swal.fire({
+            title: 'Don\'t try this please...',
+            showCancelButton: false,
+            showConfirmButton: false,
+            icon: 'info',
+            position: 'top',
+            timer: 1500,
+            timerProgressBar: 'true'
+        })
     })
 
-    passwordInput.addEventListener('cut', (event) => {
+    document.addEventListener('copy', (event) => {
         event.preventDefault()
-        alert('Nope, what is here, stay here :D')
+        Swal.fire({
+            title: 'Nope, what is here, stay here :D',
+            showCancelButton: false,
+            showConfirmButton: false,
+            icon: 'info',
+            position: 'top',
+            timer: 1500,
+            timerProgressBar: 'true'
+        })
     })
 
     if (localStorage.getItem('password')) {
         displayPassword.innerHTML = 'Your password is : ' + localStorage.getItem('password')
     }
+
+    let [red1, green1, blue1] = [0, 0, 0]
+
+    musicPlayer.addEventListener('mouseover', () => {
+        document.body.style.backgroundColor = "rgb(" + red1 + "," + green1 + "," + blue1 + ")"
+        red1 += 5
+        green1 += 4
+        blue1 += 5
+    })
+
+    let chessplate = document.querySelector('#chessplate')
+    let chessplateChilds = document.querySelectorAll('#chessplate>div')
+
+    let [red2, green2, blue2] = [15, 5, 20]
+
+    chessplateChilds.forEach(child => {
+        child.style.backgroundColor = "rgb(" + red2 + "," + green2 + "," + blue2 + ")"
+        red2 += 1
+        green2 += 1
+        blue2 += 1
+    })
+
+    let [red3, green3, blue3] = [15, 5, 20]
+    let borderSize = 0.1
+
+    chessplateChilds.forEach(child => {
+        child.addEventListener('mouseenter', () => {
+            child.style.borderWidth = borderSize + "px"
+            child.style.borderColor = "rgb(" + red3 + "," + green3 + "," + blue3 + ")"
+            red3 += 1
+            green3 += 1
+            blue3 += 5
+            borderSize += 0.1
+        })
+    })
 
     let loaderDiv = document.querySelector('#loader')
     let loader = document.querySelector('#white')
@@ -164,7 +240,8 @@ window.addEventListener('DOMContentLoaded', () => {
         passwordInput.style.display = "none"
         displayPassword.style.display = "none"
         musicPlayer.style.display = "none"
-        youtubeVideo.style.display = "none"
+        chessplate.style.display = "none"
+        textarea.style.display = "none"
         document.body.style.backgroundColor = "black"
     })
 
@@ -176,24 +253,20 @@ window.addEventListener('DOMContentLoaded', () => {
         passwordInput.style.display = "block"
         displayPassword.style.display = "block"
         musicPlayer.style.display = "block"
-        youtubeVideo.style.display = "block"
+        textarea.style.display = "block"
+        chessplate.style.display = "grid"
         document.body.style.backgroundColor = "white"
     })
 
     window.addEventListener('resize', () => {
-        alert('I just lost 2 kilos ! Stop resizing me please...')
-    })
-
-    window.addEventListener('offline', () => {
-        alert('You\'re offline !')
-    })
-
-    let [red, green, blue] = [15, 25, 250]
-
-    youtubeVideo.addEventListener('mouseover', () => {
-        document.body.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"
-        red += 2
-        green += 1
-        blue -= 2
+        Swal.fire({
+            title: 'I just lost 2 kilos ! Stop resizing me please...',
+            showCancelButton: false,
+            showConfirmButton: false,
+            icon: 'info',
+            position: 'top',
+            timer: 1500,
+            timerProgressBar: 'true'
+        })
     })
 })
